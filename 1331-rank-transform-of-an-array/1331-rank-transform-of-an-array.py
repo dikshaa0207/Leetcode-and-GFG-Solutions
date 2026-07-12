5e4808b43ff82afda1040c9be519bf442e5c847e
Diksha Singh <ds2057731@gmail.com>
2026-07-12 22:37:26 +0530

    Time: 39 ms (96.3%), Space: 27.9 MB (94.24%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.py
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.py
@@ -3,11 +3,9 @@ class Solution:
         # Sort unique elements
         unique = sorted(set(arr))
 
-        # Assign ranks
         rank = {}
         for i, num in enumerate(unique):
             rank[num] = i + 1
 
-        # Replace elements with their ranks
         return [rank[num] for num in arr]
         
